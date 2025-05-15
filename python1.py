58b6628b4250dc86ec8f37aad0bfc42df50a9536
Ali <ali.sultanov@outlook.com>
2025-05-15 22:40:42 +0200

    Updated

--- python1.py
+++ python1.py
@@ -1,1 +1,10 @@
+x = 5
+y = 10
+
+def sumOfNumbers(x,y):
+    return x+y
+
+
+print(sumOfNumbers(x,y))
+
 print("hello world")
--- python2.py
+++ python2.py
@@ -1,1 +1,9 @@
+x = 4
+y = 6
+
+def multi(x,y):
+    return x * y
+
+print(multi(x,y))
+
 print(1+1)
